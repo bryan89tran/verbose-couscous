0b98cfef0639f68ac2b38d1ca8d0232eebf741a0
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2018-06-01 22:34:07 -0700

    image state changes

---
+++ src/components/Image/Img.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class GiphyImage extends Component {
+
+    state = {
+        giphyState: false,
+        src: '',
+        animate: '',
+        title: '',
+        still: '',
+        id: ''
+    }
+
+    componentDidMount(){
+        const { title, still, animate, id } = this.props;
+        this.setState({ still, animate, title, id, src: still });
+    }
+
+    changeImage() {
+        if(this.state.giphyState === false){
+            this.setState({ src: this.state.animate, giphyState: true})
+        } else {
+            this.setState({ src: this.state.still, giphyState: false})
+        }
+    }
+
+    render(){
+        return (
+            <img 
+              src={this.state.src} 
+              alt={this.state.title}
+              id={this.state.id}
+            //   onClick={() => this.changeImage()}
+              onClick={this.changeImage.bind(this)}
+            />
+        );
+    }
+}
+
+export default GiphyImage;
